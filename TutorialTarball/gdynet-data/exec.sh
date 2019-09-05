69ba80a0c7dd0a3f44dd57b0f8cb7d77974d7d50
ajz34 <17110220038@fudan.edu.cn>
2019-09-05 17:46:10 +0800

    day 2

---
+++ TutorialTarball/gdynet-data/exec.sh
@@ -0,0 +1,4 @@
+wget https://archive.materialscloud.org/file/2019.0017/v1/li2s-traj.npz
+wget https://archive.materialscloud.org/file/2019.0017/v1/li2s-traj-graph-train.npz
+wget https://archive.materialscloud.org/file/2019.0017/v1/li2s-traj-graph-test.npz
+wget https://archive.materialscloud.org/file/2019.0017/v1/li2s-traj-graph-val.npz
